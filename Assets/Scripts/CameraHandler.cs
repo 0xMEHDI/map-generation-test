a36c1f202bdae8e55e9b608f553479538598ff1a
AccretionCD <56089902+AccretionCD@users.noreply.github.com>
2019-12-06 12:58:34 +0000

    Added CameraHandler to move camera to level's center regardless of size

---
+++ Assets/Scripts/CameraHandler.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class CameraHandler : MonoBehaviour
+{
+    [SerializeField] float followSpeed = 1f;
+
+    LevelBuilder levelBuilder;
+
+    void Awake()
+    {
+        levelBuilder = FindObjectOfType<LevelBuilder>();
+    }
+
+    void Update()
+    {
+        Vector3 targetPosition = new Vector3(5 * levelBuilder.levelSize - 5, -5 * levelBuilder.levelSize + 5, -10);
+        transform.position = Vector3.Lerp(transform.position, targetPosition, followSpeed);
+    }
+}
